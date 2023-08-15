19e888719e0468f7df9637caf5b8131ce2ad6c69
Lukas Jacomin <lukasjacomin@gmail.com>
2023-08-15 18:17:12 -0300

    fix: build script

--- build-and-run.sh
+++ build-and-run.sh
@@ -2,7 +2,7 @@
 
 docker compose -p sanepar-falta-agua up
 mkdir -p ~/sanepar-falta-agua
-cp ./.env.example ~/sanepar-falta-agua/.env
+cp -n ./.env.example ~/sanepar-falta-agua/.env
 cp ./dist/sanepar-falta-agua ~/sanepar-falta-agua/sanepar-falta-agua
 cd ~/sanepar-falta-agua
 chmod +x ./sanepar-falta-agua
